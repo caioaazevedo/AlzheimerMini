e6dc584f976bd131b8ee4421e59e538ec3de2640
GMartinz <gmdalosto@gmail.com>
2019-08-29 20:24:10 -0300

    Core saved Struct

--- AlzheimerProject/AlzheimerProject/Controller/ViewController.swift
+++ AlzheimerProject/AlzheimerProject/Controller/ViewController.swift
@@ -12,104 +12,88 @@ import CoreData
 class ViewController: UIViewController {
     
     let UserNotification = Notification()
-    var flag = 0
     
     @IBOutlet weak var feedView: UITableView!
     @IBOutlet weak var segmented: UISegmentedControl!
     
-    
-    struct eventoStruct: Hashable {
+    struct Evento: Hashable, Comparable {
         var titulo = ""
         var horario = ""
         var dia = ""
         var descricao = ""
+        
+        // Operator Overloading (Sobrecarga de Operadores)
+        static func < (lhs: Evento, rhs: Evento) -> Bool {
+            return lhs.horario < rhs.horario
+        }
     }
     
-    var eventoAux = [eventoStruct]()
+    var eventos = Set<Evento>()
+    
+    var eventosOrdenados: [Evento] {
+        get {
+            return eventos.sorted()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        UserNotification.requestNotificationAuthorization()
-        
-        
-        
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
         feedView.delegate = self
         feedView.dataSource = self
+        UserNotification.requestNotificationAuthorization()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        eventos.removeAll()
         getData()
         feedView.reloadData()
-        
     }
     
-    
     func getData(){
-        
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
         let managedContext = appDelegate.persistentContainer.viewContext
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Feed")
         
-        do{
+        do {
             let result = try managedContext.fetch(fetchRequest)
-            for data in result as! [NSManagedObject]{
+            for data in result as! [NSManagedObject] {
+                let titulo = data.value(forKey: "titulo") as! String
+                let horario = data.value(forKey: "horario") as! String
+                let dia = data.value(forKey: "dia") as! String
+                let descricao = data.value(forKey: "descricao") as! String
                 
-                let titulo = (data.value(forKey: "titulo") as! String)
+                let evento = Evento(titulo: titulo, horario: horario, dia: dia, descricao: descricao)
+                eventos.insert(evento)
                 
-                for a in eventoAux{
-                    if a.titulo == titulo{
-                        flag = 1
-                    }
-                }
-                if flag == 0{
-                    let horario = (data.value(forKey: "horario") as! String)
-                    let dia = (data.value(forKey: "dia") as! String)
-                    let descricao = (data.value(forKey: "descricao") as! String)
-                    eventoAux.append(eventoStruct(titulo: titulo, horario: horario , dia: dia , descricao: descricao))
-                    feedView.reloadData()
-                }
+                feedView.reloadData()
             }
         } catch {
             print("failed")
         }
-        
-        
-        
-        
-        
     }
     
     @IBAction func segmentedAction(_ sender: UISegmentedControl) {
         feedView.reloadData()
-        
     }
     
 }
 
 extension ViewController : UITableViewDataSource , UITableViewDelegate{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return eventoAux.count
+        return eventos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellFeed", for: indexPath) as! CellFeed
-        cell.date.text = eventoAux[indexPath.row].dia
-        cell.desc.text = eventoAux[indexPath.row].descricao
-        cell.time.text = eventoAux[indexPath.row].horario
-        cell.title.text = eventoAux[indexPath.row].titulo
+        cell.date.text = eventosOrdenados[indexPath.row].dia
+        cell.desc.text = eventosOrdenados[indexPath.row].descricao
+        cell.time.text = eventosOrdenados[indexPath.row].horario
+        cell.title.text = eventosOrdenados[indexPath.row].titulo
+
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 50
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
