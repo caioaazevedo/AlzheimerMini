124db08e8b96644481102168049fa98a0dbf9ca3
Eduardo <eduardoairton03@gmail.com>
2019-09-04 16:27:30 -0300

    Adicionando a tela de host e guest

---
+++ AlzheimerProject/AlzheimerProject/Controller/Login/GuestViewController.swift
@@ -0,0 +1,37 @@
+//
+//  GuestViewController.swift
+//  AlzheimerProject
+//
+//  Created by Eduardo Airton on 03/09/19.
+//  Copyright © 2019 Guilherme Martins Dalosto de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class GuestViewController: UIViewController{
+    
+    @IBOutlet weak var homeButton: CustomButton!
+    @IBOutlet weak var imageButton: UIButton!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        
+        
+    }
+    
+    func setUpImage() {
+        imageButton.setImage(#imageLiteral(resourceName: "loginPhoto"), for: .normal)
+        imageButton.layer.cornerRadius = imageButton.frame.size.height / 2
+    }
+    
+    
+    @IBAction func homeButton(_ sender: Any) {
+        homeButton.pulsate()
+    }
+    
+    @IBAction func imageButtonAction(_ sender: UIButton) {
+        
+    }
+    
+}
+
---
+++ AlzheimerProject/AlzheimerProject/Controller/Login/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  AlzheimerProject
+//
+//  Created by Eduardo Airton on 03/09/19.
+//  Copyright © 2019 Guilherme Martins Dalosto de Oliveira. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var hostButton: CustomButton!
+    @IBOutlet weak var guestButton: CustomButton!
+    
+    
+    @IBAction func hostButtonAction(_ sender: Any) {
+        hostButton.pulsate()
+    }
+    
+    @IBAction func guestButtonAction(_ sender: Any) {
+        guestButton.pulsate()
+    }
+    
+    
+}
