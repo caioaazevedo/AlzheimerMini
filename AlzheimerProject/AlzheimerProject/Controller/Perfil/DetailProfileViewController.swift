60c81c39070de7d534b723e4431548a799833201
GMartinz <gmdalosto@gmail.com>
2019-09-04 16:50:34 -0300

    Fix screens

---
+++ AlzheimerProject/AlzheimerProject/Controller/Perfil/DetailProfileViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DetailProfileViewController.swift
+//  AlzheimerProject
+//
+//  Created by Guilherme Martins Dalosto de Oliveira on 04/09/19.
+//  Copyright © 2019 Guilherme Martins Dalosto de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class DetailProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var idosoNome: UILabel!
+    @IBOutlet weak var dataNascimento: UILabel!
+    @IBOutlet weak var tipoSanguineo: UILabel!
+    @IBOutlet weak var telefone: UILabel!
+    @IBOutlet weak var RG: UILabel!
+    @IBOutlet weak var endereco: UILabel!
+    @IBOutlet weak var fotoIdoso: UIImageView!
+    
+    
+    
+    
+    
+    
+    
+
+}
---
+++ AlzheimerProject/AlzheimerProject/Controller/Perfil/MyProfileViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MyProfileViewController.swift
+//  AlzheimerProject
+//
+//  Created by Guilherme Martins Dalosto de Oliveira on 04/09/19.
+//  Copyright © 2019 Guilherme Martins Dalosto de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class MyProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBOutlet weak var profilePhoto: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var telefone: UILabel!
+    
+
+}
