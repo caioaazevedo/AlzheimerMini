17047a53d0935e6ea71abec8946beed52f6ccda5
GMartinz <gmdalosto@gmail.com>
2019-09-09 15:23:23 -0300

    Added screens for profile TAB

---
+++ AlzheimerProject/AlzheimerProject/Controller/Perfil/MyProfileViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MyProfileViewController.swift
+//  AlzheimerProject
+//
+//  Created by Guilherme Martins Dalosto de Oliveira on 04/09/19.
+//  Copyright © 2019 Guilherme Martins Dalosto de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class MyProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBOutlet weak var profilePhoto: UIImageView!
+    
+    
+    @IBOutlet weak var nome: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var telefone: UITextField!
+    
+    
+    
+    
+}
---
+++ AlzheimerProject/AlzheimerProject/Controller/Perfil/UsersViewController.swift
@@ -0,0 +1,49 @@
+//
+//  UsersViewController.swift
+//  AlzheimerProject
+//
+//  Created by Guilherme Martins Dalosto de Oliveira on 07/09/19.
+//  Copyright © 2019 Guilherme Martins Dalosto de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController {
+    
+    @IBOutlet weak var userstabkeView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+}
+extension UsersViewController : UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellUsers", for: indexPath) as! CellUsers
+        
+        //cell.fotoPerfil
+        cell.nomeUsuario.text = "User"
+        
+        return cell
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
